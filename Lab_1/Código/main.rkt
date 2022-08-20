ecfecffd352bd17c190182b404c737c101efbf0b
Ch3chS <sergio.espinoza.g@usach.cl>
2022-08-19 22:36:11 -0400

    Informe iniciado

---
+++ Lab_1/Código/main.rkt
@@ -0,0 +1,3 @@
+#lang racket
+
+(define hola 5)
---
+++ Lab_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSC_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
