a2882bcbf3e3e09aea977bd276d057affa9bbf5a
Sergio Espinoza <sergio.espinoza.g@usach.cl>
2022-08-21 14:03:33 -0400

    Funciones TDAs agregadas al archivo de pruebas

--- Lab_1/Código/main_20614346_EspinozaGonzalez.rkt
+++ Lab_1/Código/main_20614346_EspinozaGonzalez.rkt
@@ -2,9 +2,9 @@
 
 ;-------------------------------------------------- Importaciones ------------------------------------------------------------------------------------
 
-(require "TDAs/TDA_pixbit-d.rkt")  ;TDA pixbit-d
-(require "TDAs/TDA_pixrgb-d.rkt")  ;TDA pixrgb-d
-(require "TDAs/TDA_pixhex-d.rkt")  ;TDA pixhex-d
+(require "TDAs/TDA_pixbit-d_20614346_EspinozaGonzalez.rkt")  ;TDA pixbit-d
+(require "TDAs/TDA_pixrgb-d_20614346_EspinozaGonzalez.rkt")  ;TDA pixrgb-d
+(require "TDAs/TDA_pixhex-d_20614346_EspinozaGonzalez.rkt")  ;TDA pixhex-d
 
 ;-----------------------------------------------------------------------------------------------------------------------------------------------------
 
