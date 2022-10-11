6bb3c6eeabb40082e45f22d274ca08696f2d3c35
Ch3chS <sergio.espinoza.g@usach.cl>
2022-10-11 07:53:15 -0300

    IsCompress y Histogram agregados

---
+++ Lab_2/Código/TDAs/TDA_histogram_20614346_EspinozaGonzalez.pl
@@ -0,0 +1,23 @@
+:- module(histogram,[histogram/2, mostUsed/2]).
+
+
+% Constructor
+
+histogram([],[]).
+histogram([[_,_,Color,_]|Pixels],[[Color,Repeats]|H1]):- countColor(Color, Pixels, 1, Repeats),
+delete(Pixels, [_,_,Color,_], SubPixels),histogram(SubPixels, H1).
+
+countColor(Color,Pixels,CountIn,CountOut):- select([_,_,Color,_], Pixels, NewPixels),
+CountIn2 is CountIn + 1, countColor(Color,NewPixels,CountIn2,CountOut).
+
+countColor(_,_,CountIn,CountIn).
+
+
+% Most Used (extra)
+
+mostUsed(Pixels,Color):- histogram(Pixels, Histogram), cantList(Histogram, Cants), 
+max_member(@=<, X, Cants), once(member([Color,X],Histogram)).
+
+cantList([],[]).
+cantList([[_,Cant]|H1],[Cant|C1]):- cantList(H1,C1).
+
--- Lab_2/Código/main.pl
+++ Lab_2/Código/main.pl
@@ -1,3 +1,5 @@
+:- use_module('./TDAs/TDA_histogram_20614346_EspinozaGonzalez.pl').
+
 %pixbit
 
 pixbit-d(X,Y,Bit,Depth,[X,Y,Bit,Depth]) :- integer(X), integer(Y), integer(Bit), integer(Depth),
@@ -36,52 +38,52 @@ pixrgb_list([A|Pixels]):- pixrgb-d(_,_,_,_,_,_,A), pixrgb_list(Pixels).
 
 % ---------------------------------- 2. Constructor Image -----------------------------------------------
 
-image(Width, Height, Pixels, [Width, Height, Pixels]).
+image(Width, Height, Pixels, [0, Width, Height, Pixels, MostUsed]):- mostUsed(Pixels, MostUsed).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------- 3. IsBitmap? ----------------------------------------------------
 
-imageIsBitmap([_,_,Pixels]):- pixbit_list(Pixels).
+imageIsBitmap([_,_,_,Pixels,_]):- pixbit_list(Pixels).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------- 4. IsPixmap? ----------------------------------------------------
 
-imageIsPixmap([_,_,Pixels]):- pixrgb_list(Pixels).
+imageIsPixmap([_,_,_,Pixels,_]):- pixrgb_list(Pixels).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------- 5. IsHexmap? ----------------------------------------------------
 
-imageIsHexmap([_,_,Pixels]):- pixhex_list(Pixels).
+imageIsHexmap([_,_,_,Pixels,_]):- pixhex_list(Pixels).
 
 % -------------------------------------------------------------------------------------------------------
 
 % ---------------------------------- 6. imageIsCompressed -----------------------------------------------
 
-
+imageIsCompressed([1,_,_,_,_]).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------- 7. flipH --------------------------------------------------------
 
-imageFlipH([Width,Height,[]], [Width,Height,[]]).
-imageFlipH([Width,Height,[[X,Y,Color,Depth]|Pixels]],[Width,Height,[[NewX,Y,Color,Depth]|P1]]):- 
-NewX is Width-1-X,imageFlipH([Width,Height,Pixels],[Width, Height, P1]).
+imageFlipH([0,Width,Height,[],MostUsed], [0,Width,Height,[],MostUsed]).
+imageFlipH([0,Width,Height,[[X,Y,Color,Depth]|Pixels],MostUsed],[0,Width,Height,[[NewX,Y,Color,Depth]|P1],MostUsed]):- 
+NewX is Width-1-X,imageFlipH([0,Width,Height,Pixels,MostUsed],[0,Width, Height, P1,MostUsed]).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------- 8. flipV --------------------------------------------------------
 
-imageFlipV([Width,Height,[]], [Width,Height,[]]).
-imageFlipV([Width,Height,[[X,Y,Color,Depth]|Pixels]],[Width,Height,[[X,NewY,Color,Depth]|P1]]):- 
-NewY is Height-1-Y,imageFlipV([Width,Height,Pixels],[Width, Height, P1]).
+imageFlipV([0,Width,Height,[],MostUsed], [0,Width,Height,[],MostUsed]).
+imageFlipV([0,Width,Height,[[X,Y,Color,Depth]|Pixels],MostUsed],[0,Width,Height,[[X,NewY,Color,Depth]|P1],MostUsed]):- 
+NewY is Height-1-Y,imageFlipV([0,Width,Height,Pixels,MostUsed],[0,Width, Height, P1,MostUsed]).
 
 % -------------------------------------------------------------------------------------------------------
 
@@ -96,8 +98,8 @@ discardPixels(Pixels,X1,Y1,X2,Y2,P1).
 discardPixels([[X,Y,Color,Depth]|Pixels],X1,Y1,X2,Y2,[[X,Y,Color,Depth]|P1]):- X1 @=< X, X @=< X2, Y1 @=< Y, Y @=< Y2,
 discardPixels(Pixels,X1,Y1,X2,Y2,P1).
 
-imageCrop([_,_,Pixels],X1,Y1,X2,Y2,[Width2,Height2,Pixels2]):- Width2 is X2 - X1 + 1, Height2 is Y2 - Y1 + 1,
-discardPixels(Pixels,X1,Y1,X2,Y2,Pixels2).
+imageCrop([0,_,_,Pixels,MostUsed],X1,Y1,X2,Y2,I2):- Width2 is X2 - X1 + 1, Height2 is Y2 - Y1 + 1,
+discardPixels(Pixels,X1,Y1,X2,Y2,Pixels2), image(Width2,Height2,Pixels2,I2).
 
 % -------------------------------------------------------------------------------------------------------
 
@@ -111,14 +113,15 @@ pixelsrgbToPixelshex([],[]).
 pixelsrgbToPixelshex([Pixel|Pixels],[P1|P2]):- pixrgbToPixhex(Pixel,P1),
 pixelsrgbToPixelshex(Pixels,P2).
 
-imageRGBToHex([Width,Height,Pixels], [Width,Height,NewPixels]):- pixelsrgbToPixelshex(Pixels,NewPixels).
+imageRGBToHex([0,Width,Height,Pixels,MostUsed], I2):- pixelsrgbToPixelshex(Pixels,NewPixels),
+image(Width,Height,NewPixels,I2).
 
 % -------------------------------------------------------------------------------------------------------
 
 
 % ------------------------------------ 11. imageToHistogram ---------------------------------------------
 
-
+imageToHistogram([0,_,_,Pixels,_],Histogram):- histogram(Pixels, Histogram).
 
 % -------------------------------------------------------------------------------------------------------
 
